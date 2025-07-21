4f84e8e691f55316657928518a3a6a74fa3a8117
Akash <akashmanoj999@.com>
2025-07-21 14:19:06 +0530

    Updated variable definitions

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 module "ec2-instance" {
   source = "./modules/ec2_instance" // the path to the modules
   ami_value = "" // give the ami value
-  instance_type_value = "" // give the instance type you want eg:t2.micro
+  instance_type_value = "" // give the instance type you want eg:t2.micro,t2.medium
   
 }
 
